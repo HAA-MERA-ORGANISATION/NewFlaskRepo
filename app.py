ef22282ead7efe1cc9431401aa8ad77b4c577b2c
Ayush Agrawal <144376370+TheLearningHead@users.noreply.github.com>
2024-02-06 11:57:37 +0530

    Added hello and name routes

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/hello")
+def hello_world2():
+    return 'Hello World!'
+
+@app.route("/name")
+def name():
+    return 'Ayush'
